ced5598ef5c96da6af82841648ff6a546523ecb7
Dirza1 <jasper.olthof@xs4all.nl>
2025-06-24 08:48:05 +0200

    started implementing posts database

--- sql/queries/posts.sql
+++ sql/queries/posts.sql
@@ -1,1 +1,16 @@
-INSERT INTO posts 
+-- name: CreatePost :exec
+INSERT INTO posts (id, created_at, updated_at, title,url,description,published_at,feed_id)
+VALUES (
+    $1,
+    $2,
+    $3,
+    $4,
+    $5,
+    $6,
+    $7,
+    $8
+);
+
+-- name: GetPostsFor User :$1
+SELECT * FROM posts
+WHERE 
